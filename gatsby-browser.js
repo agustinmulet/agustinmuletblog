99e2d79025bb3dc65bf141e596ab5828a9453f57
Agustin Mulet <agustin.mulet@gmail.com>
2020-06-07 01:36:42 -0300

    Dark mode prod fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core"
+import theme from "./src/gatsby-plugin-chakra-ui/theme"
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider>
+      <CSSReset />
+      {element}
+    </ColorModeProvider>
+  </ThemeProvider>
+)
