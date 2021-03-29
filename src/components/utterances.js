068a611e0c74af91193f0de1a81e4103b5777b1e
Agustin Mulet <agustin.mulet@gmail.com>
2021-03-29 01:08:53 -0300

    Fix utteranc.es

--- src/components/utterances.js
+++ src/components/utterances.js
@@ -1,41 +1,7 @@
-/* import React, {Component} from "react";
-import ThemeContext from '../context/ThemeContext';
-export default class Comments extends Component {
-  static contextType = ThemeContext;
-
-  constructor(props){ 
-    super(props);
-    this.commentBox = React.createRef(); // Creates a reference to inject the <script> element
-  }
-  componentDidMount () {
-      const theme = this.context;
-      const utteranceTheme = theme.dark ? "github-dark" : "github-light";
-      let scriptEl = document.createElement("script");
-      scriptEl.setAttribute("src", "https://utteranc.es/client.js");
-      scriptEl.setAttribute("crossorigin","anonymous");
-      scriptEl.setAttribute("async", true);
-      scriptEl.setAttribute("repo", "vincentntang/vincentntang.com-comments");
-      scriptEl.setAttribute("issue-term", "pathname");
-      scriptEl.setAttribute( "theme", utteranceTheme);
-      this.commentBox.current.appendChild(scriptEl);
-  }
-
-  render() {
-    return (
-        <div className="comment-box-wrapper container pt-7">
-          <h1 className="mb-0">Comments</h1>
-          <hr className="my-0"/>
-          <div ref={this.commentBox} className="comment-box"/>
-        </div>
-    );
-  }
-} */
-/**/
 import React, { useEffect, useRef } from 'react'
 import { useColorMode } from "@chakra-ui/react"
 
 const src = 'https://utteranc.es/client.js'
-const branch = 'master'
 const DARK_THEME = 'icy-dark'
 const LIGHT_THEME = 'github-light'
 
